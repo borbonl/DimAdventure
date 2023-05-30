071c7c94e8696a5a8f8befdb14c69acb02c382b4
borbonl <lborbonm@hotmail.com>
2023-05-30 23:49:44 +0200

    Add files via upload

---
+++ BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(this.tag == "bulletc" && collision.tag != "Enemy" && collision.tag != "invert" && collision.gameObject.name != "Exit Zone") { 
+         Destroy(gameObject);
+        }
+
+        if (this.tag == "bullete" && collision.tag == "Player" )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
